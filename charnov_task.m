550df33298b951dab17cbbac038096ae1c58b376
bhayden <bhayden@Batcave.local>
2012-07-05 19:46:45 -0400

    saccade to make choice added

--- charnov_task.m
+++ charnov_task.m
@@ -25,6 +25,7 @@ function charnov_task(monkeysFirstInitial, trialTotal)
     % Stimuli.
     barToFixDist    = 300;            % Distance from fixation center to bar edge.
     barWidth        = 80;             % Width of all the bar stimuli.
+    currLBarHeight  = 0;              % Current height of the leave bar.
     dotRadius       = 10;             % Radius of the fixation dot.
     leaveBarHeights = [32.5  65  ...  % All the possible leave bar heights,
                        97.5  130 ...  %     which are based on shrink times
@@ -40,11 +41,13 @@ function charnov_task(monkeysFirstInitial, trialTotal)
     stayBarHeight   = 26;             % Height of the blue stay bar.
     
     % Times.
+    holdFixTime     = 0.5;            % Duration to hold fixation before choosing.
     minFixTime      = 0.2;            % Min time monkey must fixate to start trial.
     timeToFix       = 30;             % Amount of time the monkey has to fixate.
+    timeToSaccade   = intmax;         % Time allowed for monkey to make a choice.
     
     % Trial.
-    newSBarHeight   = 1;              % Whether or not new stay bar height is needed
+    newLBarHeight   = true;              % Whether or not new stay bar height is needed.
     trialType       = 1;              % Current trial. Determines bar placement.
     
     % ---------------------------------------------- %
@@ -73,42 +76,86 @@ function charnov_task(monkeysFirstInitial, trialTotal)
     
     % Displays and removes the error state on the screen.
     function error_state()
+        disp('IN ERROR STATE');
         % show error screen for 3 secs
         % restart trial
     end
     
     % Determines if the eye has fixated within the given bounds
-    % for the given duration before the given timeout occurs.
-    function fixation = check_fixation(xBoundMin, xBoundMax, ...
-                                       yBoundMin, yBoundMax, ...
-                                       duration, timeout)
+    % or the given duration before the given timeout occurs.
+    function [fixation, area] = check_fixation(xBoundMin, xBoundMax, ...
+                                               yBoundMin, yBoundMax, ...
+                                               xBoundMin2nd, xBoundMax2nd, ...
+                                               yBoundMin2nd, yBoundMax2nd, ...
+                                               duration, timeout, checkTwo)
         startTime = GetSecs;
         
         % Keep checking for fixation until timeout occurs.
         while timeout > (GetSecs - startTime)
             [xCoord, yCoord] = get_eye_coords;
             
-            % Determine if eye is within the fixation boundaries.
-            if xCoord >= xBoundMin && xCoord <= xBoundMax && ...
-               yCoord >= yBoundMin && yCoord <= yBoundMax
-                % Determine if eye maintained fixation for given duration.
-                checkFixBreak = fix_break_check(xBoundMin, xBoundMax, ...
-                                                yBoundMin, yBoundMax, ...
-                                                duration);
-                if checkFixBreak == false
-                    % Fixation was obtained for desired duration.
-                    fixation = true;
-                    
-                    return;
+            % Check for eye entering either of two locations.
+            if checkTwo
+                % Determine if eye is within either of the two fixation boundaries.
+                if (xCoord >= xBoundMin && xCoord <= xBoundMax && ...
+                    yCoord >= yBoundMin && yCoord <= yBoundMax) || ...
+                   (xCoord >= xBoundMin2nd && xCoord <= xBoundMax2nd && ...
+                    yCoord >= yBoundMin2nd && yCoord <= yBoundMax2nd)
+                    % Determine which boundary the eye entered.
+                    if xCoord >= xBoundMin && xCoord <= xBoundMax && ...
+                       yCoord >= yBoundMin && yCoord <= yBoundMax
+                        % Determine if eye maintained fixation for given duration.
+                        checkFixBreak = fix_break_check(xBoundMin, xBoundMax, ...
+                                                        yBoundMin, yBoundMax, ...
+                                                        duration);
+                        
+                        if checkFixBreak == false
+                            % Fixation was obtained for desired duration.
+                            fixation = true;
+                            area = 'first';
+
+                            return;
+                        end
+                    else
+                        % Determine if eye maintained fixation for given duration.
+                        checkFixBreak = fix_break_check(xBoundMin2nd, xBoundMax2nd, ...
+                                                        yBoundMin2nd, yBoundMax2nd, ...
+                                                        duration);
+                        
+                        if checkFixBreak == false
+                            % Fixation was obtained for desired duration.
+                            fixation = true;
+                            area = 'second';
+
+                            return;
+                        end
+                    end
+                end
+            % Otherwise just watch for eye entering one location.
+            else
+                % Determine if eye is within the fixation boundaries.
+                if xCoord >= xBoundMin && xCoord <= xBoundMax && ...
+                   yCoord >= yBoundMin && yCoord <= yBoundMax
+                    % Determine if eye maintained fixation for given duration.
+                    checkFixBreak = fix_break_check(xBoundMin, xBoundMax, ...
+                                                    yBoundMin, yBoundMax, ...
+                                                    duration);
+                    if checkFixBreak == false
+                        % Fixation was obtained for desired duration.
+                        fixation = true;
+
+                        return;
+                    end
                 end
             end
         end
         
         % Timeout reached.
         fixation = false;
+        area = 'none';
     end
 
-    % Figures out current bar position based on global variables.
+    % Figures out current bar positions based on global variables.
     function [lBXMin, lBXMax, lBYMin, lBYMax, ...
               sBXMin, sBXMax, sBYMin, sBYMax] = bar_positioning()
         % Determine positioning - leave bar: left side; stay bar: right side.
@@ -116,9 +163,16 @@ function charnov_task(monkeysFirstInitial, trialTotal)
             lBXMin = centerX - barToFixDist - barWidth;
             lBXMax = centerX - barToFixDist;
             
-            % needs to be calculated differently.
-            lBYMin = centerY - 100;
-            lBYMax = centerY + 100;
+            % Recalulate height of leave bar if needed.
+            if newLBarHeight
+                set_leave_bar;
+                
+                lBYMin = centerY - currLBarHeight / 2;
+                lBYMax = centerY + currLBarHeight / 2;
+            else
+                lBYMin = centerY - currLBarHeight / 2;
+                lBYMax = centerY + currLBarHeight / 2;
+            end
             
             sBXMin = centerX + barToFixDist;
             sBXMax = centerX + barToFixDist + barWidth;
@@ -129,9 +183,16 @@ function charnov_task(monkeysFirstInitial, trialTotal)
             lBXMin = centerX + barToFixDist;
             lBXMax = centerX + barToFixDist + barWidth;
             
-            % needs to be calculated differently.
-            lBYMin = centerY - 100;
-            lBYMax = centerY + 100;
+            % Recalulate height of leave bar if needed.
+            if newLBarHeight
+                set_leave_bar;
+                
+                lBYMin = centerY - currLBarHeight / 2;
+                lBYMax = centerY + currLBarHeight / 2;
+            else
+                lBYMin = centerY - currLBarHeight / 2;
+                lBYMax = centerY + currLBarHeight / 2;
+            end
             
             sBXMin = centerX - barToFixDist - barWidth;
             sBXMax = centerX - barToFixDist;
@@ -150,15 +211,24 @@ function charnov_task(monkeysFirstInitial, trialTotal)
     end
     
     % Draws two bars on the screen.
-    function draw_bars(leaveBarXMin, leaveBarXMax, ...
-                       leaveBarYMin, leaveBarYMax, ...
-                       stayBarXMin, stayBarXMax, ...
-                       stayBarYMin, stayBarYMax)
+    function draw_bars(leaveBarXMin, leaveBarXMax, leaveBarYMin, ...
+                       leaveBarYMax, stayBarXMin, stayBarXMax, ...
+                       stayBarYMin, stayBarYMax, fixPoint)
+        % Stay bar.
         Screen('FillRect', window, colorStayBar, [stayBarXMin stayBarYMin ...
                                                   stayBarXMax stayBarYMax]);
+        % Leave bar.
         Screen('FillRect', window, colorLeaveBar, [leaveBarXMin leaveBarYMin ...
                                                    leaveBarXMax leaveBarYMax]);
-        Screen('Flip', window);        
+        if fixPoint                           
+            % Redraw fixation point to keep it displayed.
+            Screen('FillOval', window, colorFixDot, [(centerX - dotRadius) ...
+                                             (centerY - dotRadius) ...
+                                             (centerX + dotRadius) ...
+                                             (centerY + dotRadius)]);
+        end
+        
+        Screen('Flip', window);
     end
     
     % Checks if the eye breaks fixation bounds before end of duration.
@@ -198,32 +268,63 @@ function charnov_task(monkeysFirstInitial, trialTotal)
         
         fixating = check_fixation(fixBoundXMin, fixBoundXMax, ...
                                   fixBoundYMin, fixBoundYMax, ...
-                                  minFixTime, timeToFix);
+                                  0, 0, 0, 0, ...
+                                  minFixTime, timeToFix, false);
         
         if fixating
-            % Keep fixation point on.
-            draw_fixation_point;
-            
             % Determine bar positioning.
             [lBXMin, lBXMax, lBYMin, lBYMax, ...
              sBXMin, sBXMax, sBYMin, sBYMax] = bar_positioning;
             
             % Display the bars.
             draw_bars(lBXMin, lBXMax, lBYMin, lBYMax, ...
-                      sBXMin, sBXMax, sBYMin, sBYMax);
+                      sBXMin, sBXMax, sBYMin, sBYMax, true);
+            
+            % Determine if eye maintained fixation for given duration.
+            checkFixBreak = fix_break_check(fixBoundXMin, fixBoundXMax, ...
+                                            fixBoundYMin, fixBoundYMax, ...
+                                            holdFixTime);
+            
+            % Enter error state if fixation lost, otherwise continue trial.
+            if checkFixBreak
+               error_state; 
+            else
+                % Redraw bars without fixation (remove fixation point).
+                draw_bars(lBXMin, lBXMax, lBYMin, lBYMax, ...
+                          sBXMin, sBXMax, sBYMin, sBYMax, false);
+            end
+            
+            % Find out if monkey makes a saccade to either bar.
+            [saccade, choice] = check_fixation(lBXMin, lBXMax, lBYMin, lBYMax, ...
+                                               sBXMin, sBXMax, sBYMin, sBYMax, ...
+                                               minFixTime, timeToSaccade, true);                     
+            if saccade
+                % Determine which choice monkey made.
+                if strcmp(choice, 'first')
+                    disp('FIRST CHOICE SELECTED');
+                else
+                    disp('SECOND CHOICE SELECTED');
+                end
+            else
+                error_state;
+            end
         else
             error_state;
         end
         
-        % if (fixating)
-            % draw bars with fixation point remaining
+        %* if (fixating)
+            %* draw bars with fixation point remaining
             
-            % wait for 500 ms while checking fixation continuously
-                % if fixation hasn't been broken.
-                    % remove fixation point (cue to saccade)
+            %* wait for 500 ms while checking fixation continuously
+                %* if fixation hasn't been broken.
+                    %* remove fixation point (cue to saccade)
 
-                    % continuously check monkey's eye position
-                        % if fixate on a new target==true && target==blue
+                    
+                    
+                    %* continuously check monkey's eye position
+                        % *if fixate on a new target==true && target==blue
+                        
+                        
                             % start shrinking blue bar
                             % after bar is fully shrunk,
                                 % reward the monkey with current reward val
@@ -231,16 +332,21 @@ function charnov_task(monkeysFirstInitial, trialTotal)
                                     % if reward is reduced to 0, leave at 0
                                 % ITI of 1 sec with nothing on screen
 
-                        % elseif fixate on new target==true && target==gray
+                        % *elseif fixate on new target==true && target==gray
+                        
+                            % set the makenewleavebarheight var to true
                             % start shrinking gray bar
                             % after bar is fully shrunk
                                 % clear screen
                                 % ITI of 1 sec with nothing on screen
                                 % flip trial type
                                 % reset value of gray bar
-                 % else
-                    % send error screen
-                    % restart trial
+                                
+                                
+                                
+                 %* else
+                    %* send error screen
+                    %* restart trial
     end
     
     % Rewards monkey using the juicer with the passed duration.
@@ -263,8 +369,12 @@ function charnov_task(monkeysFirstInitial, trialTotal)
         end
     end
     
-    % Determines the height, shrink rate and duration of leave bar.
+    % Randomly chooses a new height for the leave bar.
     function set_leave_bar()
+        % Get a random integer from the range 1 to 21 (inclusive).
+        randIndex = round(rand(1) * 20 + 1);
+        
+        currLBarHeight = leaveBarHeights(randIndex);
     end
     
     % Sets up the Eyelink system.
