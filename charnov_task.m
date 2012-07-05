7004ced30e1c332731590a6c4599379f6964a25a
bhayden <bhayden@Batcave.local>
2012-07-05 16:06:39 -0400

    basic bar drawing

--- charnov_task.m
+++ charnov_task.m
@@ -6,25 +6,46 @@ function charnov_task(monkeysFirstInitial, trialTotal)
     % Colors.
     colorBackground = [50 50 50];     % Background color.
     colorFixDot     = [255 255 0];    % Fixation dot color.
+    colorLeaveBar   = [175 176 175];  % Color of the stay bar stimulus.
+    colorStayBar    = [0 163 218];    % Color of the leave bar stimulus.
     
     % Coordinates.
     centerX         = 512;            % X pixel coordinate for the screen center.
     centerY         = 384;            % Y pixel coordinate for the screen center.
-    dotRadius       = 10;             % Radius of the fixation dot.
-    fixBoundXMax    = centerX + 87.5; % Max x distance from fixation point to fixate.
-    fixBoundXMin    = centerX - 87.5; % Min x distance from fixation point to fixate.
-    fixBoundYMax    = centerY + 87.5; % Max y distance from fixation point to fixate.
-    fixBoundYMin    = centerY - 87.5; % Mix y distance from fixation point to fixate.
+    fixBoundXMax    = centerX + 88;   % Max x distance from fixation point to fixate.
+    fixBoundXMin    = centerX - 88;   % Min x distance from fixation point to fixate.
+    fixBoundYMax    = centerY + 88;   % Max y distance from fixation point to fixate.
+    fixBoundYMin    = centerY - 88;   % Mix y distance from fixation point to fixate.
     
     % References.
     monkeyScreen    = 1;              % Number of the screen the monkey sees.
     trackedEye      = 2;              % Eyelink code for which eye is being tracked.
     window          = NaN;            % Reference to window used for drawing.
     
+    % Stimuli.
+    barToFixDist    = 300;            % Distance from fixation center to bar edge.
+    barWidth        = 80;             % Width of all the bar stimuli.
+    dotRadius       = 10;             % Radius of the fixation dot.
+    leaveBarHeights = [32.5  65  ...  % All the possible leave bar heights,
+                       97.5  130 ...  %     which are based on shrink times
+                       162.5 195 ...  %     ranging from 0.5 to 10.5 s in
+                       227.5 260 ...  %     intervals of 0.5 s with a shrink
+                       292.5 325 ...  %     rate of 65 pixels/s.
+                       357.5 390 ...
+                       422.5 455 ...
+                       487.5 520 ...
+                       552.5 585 ...
+                       617.5 650 ...
+                       682.5];
+    stayBarHeight   = 26;             % Height of the blue stay bar.
+    
     % Times.
     minFixTime      = 0.2;            % Min time monkey must fixate to start trial.
     timeToFix       = 30;             % Amount of time the monkey has to fixate.
     
+    % Trial.
+    newSBarHeight   = 1;              % Whether or not new stay bar height is needed
+    trialType       = 1;              % Current trial. Determines bar placement.
     
     % ---------------------------------------------- %
     % ------------------- Setup -------------------- %
@@ -65,7 +86,7 @@ function charnov_task(monkeysFirstInitial, trialTotal)
         
         % Keep checking for fixation until timeout occurs.
         while timeout > (GetSecs - startTime)
-            [xCoord yCoord] = get_eye_coords;
+            [xCoord, yCoord] = get_eye_coords;
             
             % Determine if eye is within the fixation boundaries.
             if xCoord >= xBoundMin && xCoord <= xBoundMax && ...
@@ -86,6 +107,38 @@ function charnov_task(monkeysFirstInitial, trialTotal)
         % Timeout reached.
         fixation = false;
     end
+
+    % Figures out current bar position based on global variables.
+    function [lBXMin, lBXMax, lBYMin, lBYMax, ...
+              sBXMin, sBXMax, sBYMin, sBYMax] = bar_positioning()
+        % Determine positioning - leave bar: left side; stay bar: right side.
+        if trialType == 0
+            lBXMin = centerX - barToFixDist - barWidth;
+            lBXMax = centerX - barToFixDist;
+            
+            % needs to be calculated differently.
+            lBYMin = centerY - 100;
+            lBYMax = centerY + 100;
+            
+            sBXMin = centerX + barToFixDist;
+            sBXMax = centerX + barToFixDist + barWidth;
+            sBYMin = centerY - stayBarHeight / 2;
+            sBYMax = centerY + stayBarHeight / 2;
+        % Determine positioning - leave bar: right side; stay bar: left side.
+        else
+            lBXMin = centerX + barToFixDist;
+            lBXMax = centerX + barToFixDist + barWidth;
+            
+            % needs to be calculated differently.
+            lBYMin = centerY - 100;
+            lBYMax = centerY + 100;
+            
+            sBXMin = centerX - barToFixDist - barWidth;
+            sBXMax = centerX - barToFixDist;
+            sBYMin = centerY - stayBarHeight / 2;
+            sBYMax = centerY + stayBarHeight / 2;
+        end
+    end
     
     % Draws the fixation point on the screen.
     function draw_fixation_point()
@@ -97,9 +150,15 @@ function charnov_task(monkeysFirstInitial, trialTotal)
     end
     
     % Draws two bars on the screen.
-    function draw_bars(leaveBarPos, stayBarPos, ...
-                       leaveBarHeight, stayBarHeight)
-        % Clear screen and redraw.
+    function draw_bars(leaveBarXMin, leaveBarXMax, ...
+                       leaveBarYMin, leaveBarYMax, ...
+                       stayBarXMin, stayBarXMax, ...
+                       stayBarYMin, stayBarYMax)
+        Screen('FillRect', window, colorStayBar, [stayBarXMin stayBarYMin ...
+                                                  stayBarXMax stayBarYMax]);
+        Screen('FillRect', window, colorLeaveBar, [leaveBarXMin leaveBarYMin ...
+                                                   leaveBarXMax leaveBarYMax]);
+        Screen('Flip', window);        
     end
     
     % Checks if the eye breaks fixation bounds before end of duration.
@@ -110,7 +169,7 @@ function charnov_task(monkeysFirstInitial, trialTotal)
         
         % Keep checking for fixation breaks for the entire duration.
         while duration > (GetSecs - fixStartTime)
-            [xCoord yCoord] = get_eye_coords;
+            [xCoord, yCoord] = get_eye_coords;
             
             % Determine if the eye has left the fixation boundaries.
             if xCoord < xBoundMin || xCoord > xBoundMax || ...
@@ -131,7 +190,6 @@ function charnov_task(monkeysFirstInitial, trialTotal)
         sampledPosition = Eyelink('NewestFloatSample');
         xCoord = sampledPosition.gx(trackedEye);
         yCoord = sampledPosition.gy(trackedEye);
-        disp(strcat('x: ', num2str(xCoord), '; ', 'y: ', num2str(yCoord)));
     end
     
     % Runs a single trial using current global variable values.
@@ -143,7 +201,16 @@ function charnov_task(monkeysFirstInitial, trialTotal)
                                   minFixTime, timeToFix);
         
         if fixating
+            % Keep fixation point on.
+            draw_fixation_point;
             
+            % Determine bar positioning.
+            [lBXMin, lBXMax, lBYMin, lBYMax, ...
+             sBXMin, sBXMax, sBYMin, sBYMax] = bar_positioning;
+            
+            % Display the bars.
+            draw_bars(lBXMin, lBXMax, lBYMin, lBYMax, ...
+                      sBXMin, sBXMax, sBYMin, sBYMax);
         else
             error_state;
         end
